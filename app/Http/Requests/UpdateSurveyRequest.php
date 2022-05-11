ba485559d22cfcdcc7b28296a1545e7ac58ee27b
karimjammoul <82494746+karimjammoul@users.noreply.github.com>
2022-05-12 01:58:05 +0300

    uploading project

---
+++ app/Http/Requests/UpdateSurveyRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateSurveyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $survey = $this->route('survey');
+        return $this->user()->id === $survey->user_id;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|string|max:1000',
+            'image' => 'nullable|string',
+            'user_id' => 'exists:users,id',
+            'status' => 'required|boolean',
+            'description' => 'nullable|string',
+            'expire_date' => 'nullable|date|after:tomorrow',
+            'questions' => 'array',
+        ];
+    }
+}
---
+++ vue/src/store/index.js
@@ -0,0 +1,178 @@
+import { createStore } from "vuex";
+import axiosClient from "../axios";
+
+const store = createStore({
+    state: {
+        user: {
+            data: {},
+            token: sessionStorage.getItem("TOKEN")
+        },
+        dashboard: {
+            loading: false,
+            data: {},
+        },
+        currentSurvey: {
+            loading: false,
+            data: {},
+        },
+        surveys: {
+            loading: false,
+            links: [],
+            data: [],
+        },
+        questionTypes: ['text', 'radio', 'checkbox', 'select', 'textarea'],
+        notification: {
+            show: false,
+            type: null,
+            message: null,
+        }
+    },
+    getters: {},
+    actions: {
+        register({ commit }, user) {
+            return axiosClient.post("/register", user)
+            .then(({ data }) => {
+                commit("setUser", data);
+                return data
+            });
+        },
+        login({ commit }, user) {
+            return axiosClient.post("/login", user)
+            .then(({ data }) => {
+                commit("setUser", data);
+                return data
+            });
+        },
+        logout({ commit }) {
+            return axiosClient.post("/logout")
+            .then(response => {
+                commit('logout');
+                return response;
+            })
+        },
+        getSurvey({ commit }, id) {
+            commit("setCurrentSurveyLoading", true);
+            return axiosClient.get(`/survey/${id}`)
+            .then((res) => {
+                commit("setCurrentSurvey", res.data);
+                commit("setCurrentSurveyLoading", false);
+                return res;
+            })
+            .catch((err) => {
+                commit("setCurrentSurveyLoading", false);
+                throw err;
+            })
+        },
+        saveSurvey({ commit }, survey) {
+            commit("setCurrentSurveyLoading", true);
+            delete survey.image_url;
+            let response;
+            if(survey.id) {
+                response = axiosClient
+                    .put(`/survey/${survey.id}`, survey)
+                    .then( (res) => {
+                        commit('setCurrentSurvey', res.data);
+                        commit("setCurrentSurveyLoading", false);
+                        return res;
+                    })
+            }
+            else {
+                response = axiosClient
+                    .post(`/survey`, survey)
+                    .then( (res) => {
+                        commit('setCurrentSurvey', res.data);
+                        commit("setCurrentSurveyLoading", false);
+                        return res;
+                    })
+            }
+            return response;
+        },
+        deleteSurvey({}, id) {
+            return axiosClient.delete(`/survey/${id}`)
+        },
+        getSurveys({ commit }, { url = null } = {}) {
+            url = url || `/survey`;
+            commit("setSurveysLoading", true);
+            return axiosClient.get(url)
+            .then((res) => {
+                commit("setSurveysLoading", false);
+                commit("setSurveys", res.data);
+                return res;
+            }
+            ).catch((err) => {
+                commit("setSurveysLoading", false);
+                throw err;
+            })
+        },
+        getSurveyBySlug({ commit }, slug) {
+            commit("setCurrentSurveyLoading", true);
+            return axiosClient
+            .get(`/survey-by-slug/${slug}`)
+            .then((res) => {
+                commit("setCurrentSurvey", res.data);
+                commit("setCurrentSurveyLoading", false);
+                return res;
+            })
+            .catch((err) => {
+                commit("setCurrentSurveyLoading", false);
+                throw err;
+            });
+        },
+        saveSurveyAnswer({commit}, {surveyId, answers}) {
+            return axiosClient.post(`/survey/${surveyId}/answer`, {answers});
+        },
+        getDashboardData({ commit }) {
+            commit("setDashboardLoading", true);
+            return axiosClient.get("/dashboard")
+            .then((res) => {
+                commit("setDashboardData", res.data);
+                commit("setDashboardLoading", false);
+                return res;
+            }).catch((err) => {
+                commit("setDashboardLoading", false);
+                throw err;
+            })
+        },
+    },
+    mutations: {
+        logout: (state) => {
+            state.user.data = {};
+            state.user.token = null;
+        },
+        setUser: (state, userData) => {
+            state.user.token = userData.token;
+            state.user.data = userData;
+            sessionStorage.setItem("TOKEN", userData.token);
+        },
+        setCurrentSurveyLoading: (state, loading) => {
+            state.currentSurvey.loading = loading;
+        },
+        setCurrentSurvey: (state, survey) => {
+            state.currentSurvey.data = survey.data;
+        },
+        setSurveysLoading: (state, loading) => {
+            state.surveys.loading = loading;
+        },
+        setSurveys: (state, surveys) => {
+            state.surveys.links = surveys.meta.links;
+            state.surveys.data = surveys.data;
+        },
+        notify: (state, { type, message }) => {
+            state.notification.show = true;
+            state.notification.type = type;
+            state.notification.message = message;
+            setTimeout(() => {
+                state.notification.show = false;
+            }, 3000);
+        },
+        setDashboardLoading: (state, loading) => {
+            state.dashboard.loading = loading;
+        },
+        setDashboardData: (state, data) => {
+            state.dashboard.data = data;
+        },
+    },
+    modules: {},
+});
+
+export default store;
